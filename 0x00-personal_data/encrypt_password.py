ff0daf61d67c710720233958511bb0e67b5eb2e9
beni-f <benifissha@gmail.com>
2024-07-04 20:57:35 +0300

    encrypt_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,6 @@
 Encrypting passwords
 """
 import bcrypt
-from typing import ByteString
 
 
 def hash_password(password: str) -> bytes:
@@ -11,3 +10,10 @@ def hash_password(password: str) -> bytes:
         returns a salted, hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Validates if password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
