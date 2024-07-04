be097af1c3d6624b7fbf6b423a0ded00d48518d4
beni-f <benifissha@gmail.com>
2024-07-04 19:21:09 +0300

    filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,10 @@ import logging
 from typing import List
 
 
+PII_FIELDS = (
+    "name", "email", "phone", "ssn", "password"
+)
+
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
@@ -17,6 +21,16 @@ def filter_datum(
     return re.sub(
         pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message
     )
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter(RedactingFormatter(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
@@ -33,6 +47,8 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Filters incoming log records using the filter_datum function.
+        """
         record.msg = (filter_datum(
             self.fields, self.REDACTION, record.msg, self.SEPARATOR))
         return super(RedactingFormatter, self).format(record)
