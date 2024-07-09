d7d767a72383a13fbc3abcf3adfa6267d644b6da
beni-f <benifissha@gmail.com>
2024-07-09 20:05:53 +0300

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Auth Class
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+            Not used yet.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+            Not used yet.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """
+            Not used yet.
+        """
+        return None
