f97ca5a67ccf3443ef7d5ffaeee64a6acf51bba4
beni-f <benifissha@gmail.com>
2024-07-11 08:39:00 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,21 @@
 Session Auth
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """"
         Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            Generates a session id for a user given by user_id
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        key = uuid.uuid4()
+        self.user_id_by_session_id[key] = user_id
+        return key
