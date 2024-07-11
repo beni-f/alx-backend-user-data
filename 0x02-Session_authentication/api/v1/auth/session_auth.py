a6e091e50e039d2730155db8b814fdd8658c24c3
beni-f <benifissha@gmail.com>
2024-07-11 19:03:48 +0300

    updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,7 +21,7 @@ class SessionAuth(Auth):
         """
         if not user_id or not isinstance(user_id, str):
             return None
-        key = uuid.uuid4()
+        key = str(uuid.uuid4())
         self.user_id_by_session_id[key] = user_id
         return key
 
@@ -31,7 +31,7 @@ class SessionAuth(Auth):
         """
         if not session_id:
             return None
-        return self.user_id_by_session_id.get(uuid.UUID(session_id))
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """
@@ -40,10 +40,6 @@ class SessionAuth(Auth):
         if request is None:
             return None
         session_id = self.session_cookie(request)
-        if not session_id:
-            return None
         user_id = self.user_id_for_session_id(session_id)
-        if not user_id:
-            return None
         user = User.get(user_id)
         return user
