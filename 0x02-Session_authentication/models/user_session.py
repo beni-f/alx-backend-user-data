3bb74ff60d617ad7b0a555bd02fa9c12951307a3
beni-f <benifissha@gmail.com>
2024-07-13 22:08:37 +0300

    updated

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,6 +10,9 @@ class UserSession(Base):
         Database to store Session ID besides other user information
     """
     def __init__(self, *args: list, **kwargs: dict):
+        """
+            Initialization
+        """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
