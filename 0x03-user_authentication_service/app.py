12b846444d769ea0a374dc96bea01f30ae1cd402
beni-f <benifissha@gmail.com>
2024-07-17 20:46:45 +0300

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,17 +43,18 @@ def login() -> str:
         abort(401)
 
 
-@app.route('/sessions', methods=['DELETE'])
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
+    """DELETE /sessions
+    Return:
+        - Redirects to home route.
     """
-        Log Out
-    """
-    session_id = request.form.get('session_id')
-    user = AUTH._db.find_user_by(session_id=session_id)
-    if not user:
-        return None
-    AUTH.destory_session(user.id)
-    return redirect('/')
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
