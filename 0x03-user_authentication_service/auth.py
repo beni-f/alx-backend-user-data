87d155d4ff3f7760dc31533f843ccaba8d12cfe2
beni-f <benifissha@gmail.com>
2024-07-17 16:38:41 +0300

    Register User

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,29 @@
 import bcrypt
+from db import DB
+from user import User
+
 def _hash_password(password):
+    """
+        Hash a password 
+    """
+    password = password.encode('utf-8')
+    return bcrypt.hashpw(password, salt=bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email, password):
         """
-           Hash a password 
+        Takes email and password as an argument
+        then checks if email already exists or not and register user accordingly.
         """
-        password = password.encode('utf-8')
-        return bcrypt.hashpw(password, salt=bcrypt.gensalt())
+        usr_email = self._db._session.query(User).filter_by(email=email).first()
+        if usr_email:
+            raise ValueError(f'User {email} already exists')
+        password = _hash_password(password)
+        user = self._db.add_user(email, password)
+        return user
