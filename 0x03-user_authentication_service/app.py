dc09b0231645fe40e26629ef6af560ba5bbc7419
beni-f <benifissha@gmail.com>
2024-07-17 20:40:12 +0300

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,9 @@
 """
 Basic Flask App
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
@@ -27,18 +28,27 @@ def users():
         return jsonify({'message': 'email already registered'}), 400
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST', 'DELETE'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    is_valid = AUTH.valid_login(email, password)
-    if is_valid:
-        session_id = AUTH.create_session(email)
-        response = jsonify({'email': email, 'message': 'logged in'})
-        response.set_cookie('session_id', session_id)
-        return response
-    else:
-        abort(401)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        is_valid = AUTH.valid_login(email, password)
+        if is_valid:
+            session_id = AUTH.create_session(email)
+            response = jsonify({'email': email, 'message': 'logged in'})
+            response.set_cookie('session_id', session_id)
+            return response
+        else:
+            abort(401)
+    elif request.method == 'DELETE':
+        session_id = request.form.get('session_id')
+        try:
+            user = AUTH._db.find_user_by(session_id=session_id)
+            AUTH.destory_session(user.id)
+            return redirect('/')
+        except NoResultFound:
+            abort(403)
 
 
 if __name__ == "__main__":
