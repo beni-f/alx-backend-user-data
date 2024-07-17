ad2f2bf3af35d0f2c1d8e2b721b1b88f6f289eb7
beni-f <benifissha@gmail.com>
2024-07-17 17:03:27 +0300

    updated user

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,25 +2,9 @@
 """
 Main file
 """
-import logging
-from auth import Auth
+from user import User
 
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+print(User.__tablename__)
 
-email = 'me@me.com'
-password = 'mySecuredPwd'
-
-auth = Auth()
-
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
-
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))    
+for column in User.__table__.columns:
+    print("{}: {}".format(column, column.type))
