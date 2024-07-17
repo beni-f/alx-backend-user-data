1301c4b0f12c48aedbe2dc0147fa4497bb13cb45
beni-f <benifissha@gmail.com>
2024-07-17 20:50:07 +0300

    updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,17 +79,16 @@ class Auth:
         return session_id
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Retrieves a user based on a given session ID.
         """
-            Find user by session ID
-        """
-        usr = None
-        if not session_id:
+        user = None
+        if session_id is None:
             return None
         try:
-            usr = self._db.find_user_by(session_id=session_id)
+            user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-        return usr
+        return user
 
     def destroy_session(self, user_id: int) -> None:
         """Destroys a session associated with a given user.
