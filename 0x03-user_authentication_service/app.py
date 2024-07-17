6075fa999409b9704daf824267ed1856acbb659a
beni-f <benifissha@gmail.com>
2024-07-17 20:43:10 +0300

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
+app.strict_slashes = False
 AUTH = Auth()
 
 
@@ -28,27 +29,32 @@ def users():
         return jsonify({'message': 'email already registered'}), 400
 
 
-@app.route('/sessions', methods=['POST', 'DELETE'])
+@app.route('/sessions', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        is_valid = AUTH.valid_login(email, password)
-        if is_valid:
-            session_id = AUTH.create_session(email)
-            response = jsonify({'email': email, 'message': 'logged in'})
-            response.set_cookie('session_id', session_id)
-            return response
-        else:
-            abort(401)
-    elif request.method == 'DELETE':
-        session_id = request.form.get('session_id')
-        try:
-            user = AUTH._db.find_user_by(session_id=session_id)
-            AUTH.destory_session(user.id)
-            return redirect('/')
-        except NoResultFound:
-            abort(403)
+    email = request.form.get('email')
+    password = request.form.get('password')
+    is_valid = AUTH.valid_login(email, password)
+    if is_valid:
+        session_id = AUTH.create_session(email)
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """
+        Log Out
+    """
+    session_id = request.form.get('session_id')
+    try:
+        user = AUTH._db.find_user_by(session_id=session_id)
+        AUTH.destory_session(user.id)
+        return redirect('/')
+    except NoResultFound:
+        abort(403)
 
 
 if __name__ == "__main__":
