3f5d374a36a738953abbc093133b955f7a84c44b
beni-f <benifissha@gmail.com>
2024-07-18 20:25:13 +0300

    updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -98,7 +98,7 @@ def update_password() -> str:
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-    except NoResultFound:
+    except ValueError:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"}), 200
 
