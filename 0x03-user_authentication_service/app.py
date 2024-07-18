af315c27523dc507cbf26bc910bc0975f3f23d96
beni-f <benifissha@gmail.com>
2024-07-18 18:56:51 +0300

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,20 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def reset_password():
+    """
+        Reset Password
+    """
+    email = request.form.get('email')
+    try:
+        user = AUTH._db.find_user_by(email=email)
+        reset_token = AUTH.get_reset_password_token(email)
+        response = jsonify({'email': email, 'reset_token': reset_token})
+        return response, 200
+    except NoResultFound:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
